2dea2dfe1ec827713bc3e5444ff000161eb4d116
Arthur Serra <arthur.serra10@gmail.com>
2018-12-08 20:12:29 -0200

    Update BRKGA.py

--- BRKGA.py
+++ BRKGA.py
@@ -1,52 +1,23 @@
-import random
+import BRKGA
+import graph as gr
+import individuo as ind
 import populacao as pp
-class BRKGA:
-    taxaCruzamento = 0.7
-    taxaMutacao = 0.21
-    taxaSobrevivencia = 0.2
-    ELITISM = True
-    elite = []
-    n_VERT = 0
-    n_COLOR = 0
-    tamPopulacao = 5
+import time
 
-    def Evoluir(self, Poulacao, numVert, numColor, edges):
-        n_VERT = numVert
-        n_COLOR = numColor
-        novaPopulacao = pp.Population(tamPopulacao, True, numVert, numColor, edges)
-        sobrevive = 0
-        if (ELITISM):
-            elite = novaPopulacao.getEliteFromFitness(taxaSobrevivencia)
+if __name__ == "__main__":
 
-            for i in range(len(elite)):
-                novaPopulacao.coloring[i] = elite[i]
-                sobrevive += 1
-        for i in range(sobrevive, novaPopulacao.tamPopulacao):
-            taxa = int(100*taxaCruzamento)
-            if randint(0, 100) < taxa:
-                positPai = randint(0, len(elite))
-                filho = self.crossover(elite[positPai], Populacao.getColoring(i))
-                novaPopulacao.saveColoring(novaPopulacao)
+    tempoLimite = 0.5 #tempo em minutos
+    tempo = time.time()
+    grafo = gr.Graph(5)
+    Populacao = pp.Population(10, True, grafo.numVert, grafo.numColor, grafo.edges)
+    TAXA = BRKGA.BRKGA()
+    print()
+    geracoes = 0
+    while (int(time.time()- tempo)/60) < tempoLimite:
+        Populacao = TAXA.Evoluir(Populacao, Populacao.tamPopulacao, grafo.numVert, grafo.numColor, grafo.edges)
+        geracoes += 1
 
-        for i in range(sobrevive, novaPopulacao.tamPopulacao):
-            taxa = int(100*taxaMutacao)
-            if randint(0, 100) < taxa:
-                self.mutacao(novaPopulacao.getColoring(i))
-
-        return novaPopulacao
-
-    def mutacao(ind):
-        for i in range(len(ind.chromosome)):
-            taxa = int(100*taxaMutacao)
-            if randint(0, 100) < taxa:
-                self.mutacao(novaPopulacao.getColoring(i))
-                color = randint(0, n_COLOR)
-                ind.setColorChromosome(i, color)
-
-    def crossover(ind1, ind2):
-        filho = ind1
-        for i in range(len(ind1.chromosome)):
-            if i%2 == 0:
-                filho.setColorChromosome(i, ind2.chromosome[i])
-x = BRKGA()
-print(type(x))
+    elite = Populacao.getEliteFromFitness(TAXA.taxaSobrevivencia)
+    coresUsadas  = elite[0].colors
+    print("Total de cores usadas => ", coresUsadas)
+    print("Total de gerações -> ", geracoes)
