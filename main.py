f1c8421eca8ec9992e78a01a57684ae77fbc20f0
Arthur Serra <arthur.serra10@gmail.com>
2018-12-08 20:15:38 -0200

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,22 @@ import BRKGA
 import graph as gr
 import individuo as ind
 import populacao as pp
+import time
 
-def __name__ == "__main__":
+if __name__ == "__main__":
+
+    tempoLimite = 0.5 #tempo em minutos
+    tempo = time.time()
+    grafo = gr.Graph(5)
+    Populacao = pp.Population(10, True, grafo.numVert, grafo.numColor, grafo.edges)
+    TAXA = BRKGA.BRKGA()
+    print()
+    geracoes = 0
+    while (int(time.time()- tempo)/60) < tempoLimite:
+        Populacao = TAXA.Evoluir(Populacao, Populacao.tamPopulacao, grafo.numVert, grafo.numColor, grafo.edges)
+        geracoes += 1
+
+    elite = Populacao.getEliteFromFitness(TAXA.taxaSobrevivencia)
+    coresUsadas  = elite[0].colors
+    print("Total de cores usadas => ", coresUsadas)
+    print("Total de gerações -> ", geracoes)
